e303039e24305871c82763bf891a1b5a7ede14cc
Hadar5325 <55797369+Hadar5325@users.noreply.github.com>
2020-01-05 23:55:46 +0200

    add static to function

---
+++ PrintCommand.h
@@ -0,0 +1,14 @@
+
+#ifndef UNTITLED1_PRINTCOMMAND_H
+#define UNTITLED1_PRINTCOMMAND_H
+#include <string>
+
+using namespace std;
+
+class PrintCommand {
+private:
+public:
+    static void printText(string str);
+};
+
+#endif //UNTITLED1_PRINTCOMMAND_H
